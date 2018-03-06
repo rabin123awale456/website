912f23a5abe02c1c75139b930e2a20c2fab17a38
rabin <rabin123awale456@gmail.com>
2018-03-05 17:20:21 -0800

    added bootstraap framwork

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 <html>
 <head>
 	<title>website</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 </head>
 <body>
 	<h1>hello rabin are u fine</h1>
-
+<p>sagddsgsa</p>
 </body>
 </html>
+
